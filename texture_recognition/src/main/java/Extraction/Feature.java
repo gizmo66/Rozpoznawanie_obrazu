a341d331883268a12bcfbe02991d83373a1efbeb
Adam Kołodziejek <adam.kolodziejek@comarch.pl>
2017-11-30 21:21:36 +0100

    refactor

--- texture_recognition/src/main/java/Extraction/Feature.java
+++ texture_recognition/src/main/java/Extraction/Feature.java
@@ -5,8 +5,9 @@ public interface Feature {
     String CALCULATE_FEATURE_METHOD = "calculateValue";
     String GET_FEATURE_NAME_METHOD = "getFeatureName";
 
-    FeatureNameEnum featureName = FeatureNameEnum.DEFAULT;
-
     Number calculateValue(Picture picture);
-    String getFeatureName();
+
+    default String getFeatureName() {
+        return this.getClass().getSimpleName();
+    }
 }
